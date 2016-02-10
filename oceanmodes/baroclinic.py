40acfd66d7082af89cf2a02cdd6d48aa851c3284
Takaya Uchida <takaya@vislab-nas.ldeo.columbia.edu>
2016-02-10 15:36:14 -0500

    Tests passed for linear stability

--- oceanmodes/baroclinic.py
+++ oceanmodes/baroclinic.py
@@ -4,11 +4,12 @@ from builtins import *
 #
 import numpy as np
 from scipy import fftpack as fft
-from scipy.sparse import lil_matrix
-from scipy.sparse.linalg import eigs, inv
+from scipy.sparse import lil_matrix, csc_matrix
+from scipy.sparse.linalg import eigs, inv, dot
 
 def _maybe_truncate_above_topography(z, f):
-    "Return truncated versions of z and f if f is masked or nan."
+    """Return truncated versions of z and f if f is masked or nan.
+    """
     # checks on shapes of stuff
     if not z.shape == f.shape:
         raise ValueError('z and f must have the same length')
@@ -29,43 +30,44 @@ def _maybe_truncate_above_topography(z, f):
     return zout, fout
 
 def neutral_modes_from_N2_profile(z, N2, f0, depth=None, **kwargs):
-    """Calculate baroclinic neutral modes from a profile of buoyancy frequency.
+    """Calculate baroclinic neutral modes from a profile of 
+        buoyancy frequency truncated at the bottom.
 
-    Solves the eigenvalue problem
+        Solves the eigenvalue problem
 
-        \frac{d}{dz}\left ( \frac{f_0^2}{N^2} \frac{d \phi}{d z} \right )
-        = -\frac{1}{R_d^2} \phi
+            \frac{d}{dz}\left ( \frac{f_0^2}{N^2} \frac{d \phi}{d z} \right )
+            = -\frac{1}{R_d^2} \phi
 
-    With the boundary conditions
+        With the boundary conditions
 
-        \frac{d \phi}{d z} = 0
+            \frac{d \phi}{d z} = 0
 
-    at the top and bottom.
+        at the top and bottom.
 
-    Parameters
-    ----------
-    z : array_like
-        The depths at which N2 is given. Starts shallow, increases
-        positive downward.
-    N2 : array_like
-        The squared buoyancy frequency (units s^-2). Points below topography
-        should be masked or nan.
-    f0 : float
-        Coriolis parameter.
-    depth : float, optional
-        Total water column depth. If missing will be inferred from z.
-    kwargs : optional
-        Additional parameters to pass to scipy.sparse.linalg.eigs for
-        eigenvalue computation
+        Parameters
+        ----------
+        z : array_like
+            The depths at which N2 is given. Starts shallow, increases
+            positive downward.
+        N2 : array_like
+            The squared buoyancy frequency (units s^-2). Points below topography
+            should be masked or nan.
+        f0 : float
+            Coriolis parameter.
+        depth : float, optional
+            Total water column depth. If missing will be inferred from z.
+        kwargs : optional
+            Additional parameters to pass to scipy.sparse.linalg.eigs for
+            eigenvalue computation
 
-    Returns
-    -------
-    zc : array_like
-        The depths at which phi is defined. Different from z.
-    R_d : array_like
-        deformation radii, sorted descending
-    phi : array_like
-        vertical modes
+        Returns
+        -------
+        zf : array_like
+            The depths at which phi is defined. Different from z.
+        R_d : array_like
+            deformation radii, sorted descending
+        phi : array_like
+            vertical modes
     """
     nz_orig = len(z)
     z, N2 = _maybe_truncate_above_topography(z, N2)
@@ -84,9 +86,9 @@ def _neutral_modes_from_N2_profile_raw(z, N2, f0, depth=None, **kwargs):
     #
     # ----- zf[1], phi[1] -------
     # ...
-    # ---- zc[nz-1], N2[nz-1] ---
+    # ----- zc[nz-1], N2[nz-1] ---
     #
-    # ~~~~ zf[nz], phi[nz] ~~~~~~
+    # ~~~~~ zf[nz], phi[nz] ~~~~~
 
     # just for notation's sake
     # (user shouldn't worry about discretization)
@@ -174,50 +176,65 @@ def _neutral_modes_from_N2_profile_raw(z, N2, f0, depth=None, **kwargs):
 
     return zf, Rd, v
 
-def _instability_analysis_from_N2_profile_raw(z, N2, f0, beta, Nx, Ny, dx, dy, ubar, vbar, etax, etay, depth=None, **kwargs):
+def instability_analysis_from_N2_profile(z, N2, f0, beta, Nx, Ny, dx, dy, ubar, vbar, etax, etay, sort='LI', num=4, depth=None, **kwargs):
+    """Calculate baroclinic neutral modes from a profile of 
+        buoyancy frequency truncated at the bottom.
+    """
+    nz_orig = len(z)
+    z, N2 = _maybe_truncate_above_topography(z, N2)
+    return _instability_analysis_from_N2_profile_raw(
+                                    z, N2, f0, beta, Nx, Ny, dx, dy, ubar, vbar, etax, etay, sort, num, depth=depth, **kwargs)
+
+def _instability_analysis_from_N2_profile_raw(z, N2, f0, beta, Nx, Ny, dx, dy, ubar, vbar, etax, etay, sort='LI', num=4, depth=None, **kwargs):
     """Calculate baroclinic unstable modes from a profile of buoyancy frequency.
     
-    Solves the eigenvalue problem
+        Solves the eigenvalue problem
 
-        \left ( \frac{\partial}{\partial t} + U \cdot \nabla \right ) \left ( \nabla^2 + \frac{d}{dz} \frac{f_0^2}{N^2} \frac{d \psi}{d z} \right )
-        + u \cdot \nabla Q
-        = 0
+            \left ( \frac{\partial}{\partial t} + U \cdot \nabla \right ) \left ( \nabla^2 + \frac{d}{dz} \frac{f_0^2}{N^2} \frac{d \psi}{d z} \right )
+            + u \cdot \nabla Q
+            = 0
 
-    With the boundary conditions
+        With the boundary conditions
 
-        \left ( \frac{\partial}{\partial t} + U \cdot \nabla \right ) f_0 \frac{\partial \psi}{\partial z}
-        + u \cdot \nabla \left ( B + N^2 \eta \right )
-        = 0
+            \left ( \frac{\partial}{\partial t} + U \cdot \nabla \right ) f_0 \frac{\partial \psi}{\partial z}
+            + u \cdot \nabla \left ( B + N^2 \eta \right )
+            = 0
 
-    at the top and bottom.
-
-    Parameters
-    ----------
-    z : array_like
-        The depths at which N2 is given. Starts shallow, increases
-        positive downward.
-    N2 : array_like
-        The squared buoyancy frequency (units s^-2). Points below topography
-        should be masked or nan.
-    f0 : float
-        Coriolis parameter.
-    Q : array_like
-        The background QGPV (\beta y + \left ( \nabla^2 + \frac{d}{dz} \frac{f_0^2}{N^2} \frac{d \Psi}{d z} \right ))
-        and Psi is the streamfunction of the background velocity.
-    eta: array_like
-        The topographic change at the top and bottom.
-    depth : float, optional
-        Total water column depth. If missing will be inferred from z.
-    kwargs : optional
-        Additional parameters to pass to scipy.sparse.linalg.eigs for
-        eigenvalue computation
-
-    Returns
-    -------
-    zc : array_like
-        The depths at which phi is defined. Different from z.
-    psi : array_like
-        vertical modes
+        at the top and bottom.
+    
+        Parameters
+        ----------
+        z : array_like
+            The depths at which N2 is given. Starts shallow, increases
+            positive downward.
+        N2 : array_like
+            The squared buoyancy frequency (units s^-2). Points below topography
+            should be masked or nan.
+        f0 : float
+            Coriolis parameter.
+        Q : array_like
+            The background QGPV (\beta y + \left ( \nabla^2 + \frac{d}{dz} \frac{f_0^2}{N^2} \frac{d \Psi}{d z} \right ))
+            and Psi is the streamfunction of the background velocity.
+        eta : array_like
+            The topographic change at the top and bottom.
+        B : array_like
+            The background buoyancy
+        depth : float, optional
+            Total water column depth. If missing will be inferred from z.
+        kwargs : optional
+            Additional parameters to pass to scipy.sparse.linalg.eigs for
+            eigenvalue computation
+    
+        Returns
+        -------
+        zf : array_like
+            The depths at which phi is defined. Different from z.
+        psi : array_like
+            vertical modes
+        omega: array_like
+            growth rate
+        k, l : array_like
+            inverse horizontal wavelengths
     """
     nz = len(z)
 
@@ -254,7 +271,7 @@ def _instability_analysis_from_N2_profile_raw(z, N2, f0, beta, Nx, Ny, dx, dy, u
     dzf = np.diff(zf)
     
     # We want a matrix representation of the operator such that
-    #    np.dot(G, f) = g = np.dot(L, f)
+    #    np.dot(L, f) = g = np.dot(G, f)
     # This can be put in "tridiagonal" form
     # 1) first derivative of f (defined at zf points, maps to zc points)
     #    dfdz[i] = (f[i] - f[i+1]) /  dzf[i]
@@ -286,108 +303,105 @@ def _instability_analysis_from_N2_profile_raw(z, N2, f0, beta, Nx, Ny, dx, dy, u
     #    a = (kU[i] + lV[i]) * (dzf[0] * N2[0] * dztop)**-1  - S[0] 
     #    b = (kU[nz] + lV[nz]) * (dzf[nz-1] * N2[nz-1])**-1 * dzbot) - S[nz] 
     
-    k = fft.fftfreq(Nx, dx)
-    l = fft.fftfreq(Ny, dy)
+    k = fft.fftshift( fft.fftfreq(Nx, dx) )
+    l = fft.fftshift( fft.fftfreq(Ny, dy) )
     
-    omega = np.zeros((4, len(l), len(k)))
-    psi1 = np.zeros((nz, len(l), len(k)))
-    psi2 = np.zeros((nz, len(l), len(k)))
-    psi3 = np.zeros((nz, len(l), len(k)))
-    psi4 = np.zeros((nz, len(l), len(k)))
-    
-    for j in range(len(l)/2):
-        for i in range(len(k)/2):
-            
-            AC = lil_matrix((nz+1, nz+1), dtype=np.float64)
-            BD = AC.copy()
-            
-            for n in range(nz):
+    for j in range(len(l)):
+        for i in range(len(k)):
+        
+            #print i, j, k[i], l[j]
+        
+            L = lil_matrix((nz+1, nz+1), dtype=np.float64)
+            G = L.copy()
                 
+            for n in range(nz+1):
+                    
+                R = ( k[i]*ubar[n] + l[j]*vbar[n] )
+#             print R
+
                 if n == 0:
-                    
-                    D = dzf[n]**-1
-                    S = ( k[i] * ( (ubar[n+1]-ubar[n])*D - N2[n]/f_0**2 * etay[0] )
-                                + l[j] * ( (vbar[n+1]-vbar[n])*D - N2[n]/f_0**2 * etax[0] ) )
-                    
-                    AC[n, n] = ( ( k[i]*ubar[n] + l[j]*vbar[n] ) * ( -D ) 
-                                - S
-                                 )
-                    AC[n, n+1] = ( ( k[i]*ubar[n] + l[j]*vbar[n] ) * D 
-                                - S
-                                 )
-                    BD[n, n] = - D
-                    BD[n, n+1] = D
-                    
-                elif n > 0 and n < nz-1:
-                    
+    
+                    D = .5 * dzf[n]**-1
+                    S = ( k[i] * ( (ubar[n+1]-ubar[n])*D - N2[n]/f0 * etay[0] )
+                                        + l[j] * ( (vbar[n+1]-vbar[n])*D + N2[n]/f0 * etax[0] ) )
+#                 print D, S
+
+                    L[n, n] = R * ( -D ) - S
+                    L[n, n+1] = R * D - S
+                    G[n, n] = - D
+                    G[n, n+1] = D
+    
+                elif n > 0 and n < nz:
+    
                     K2 = k[i]**2 + l[j]**2
-                    bf = f_0**2 * dzc[p]**-1
-                    b_1 = N2[p-1] * dzf[p-1]
-                    b = N2[p] * dzf[p]
+                    bf = f0**2 * dzc[n-1]**-1
+                    b_1 = N2[n-1] * dzf[n-1]
+                    b = N2[n] * dzf[n]
                     B_1 = bf * b_1**-1 - K2
                     B = - bf * ( b**-1 + b_1**-1 ) - K2
                     Bt1 = bf * b**-1 - K2
-                    
-                    D = dzc[n]**-1
+    
                     N2Z = (N2[n]*dzf[n])**-1
                     N2Z_1 = (N2[n-1]*dzf[n-1])**-1
-                    P = ( k[i] * ( beta - f_0**2 * D * ( ubar[n+1] * N2Z
-                                                        - (N2Z+N2Z_1) * ubar[n]
-                                                        + N2Z_1 * ubar[n-1] ) )
-                            - l[j] * f_0**2 * D * ( vbar[n+1] * N2Z 
-                                                   - (N2Z+N2Z_1) * vbar[n]
-                                                   + N2Z_1 * vbar[n-1] )
+                    P = ( k[i] * ( beta - bf * ( ubar[n+1] * N2Z
+                                                                - (N2Z + N2Z_1) * ubar[n]
+                                                                + N2Z_1 * ubar[n-1] ) )
+                         - l[j] * bf * ( vbar[n+1] * N2Z 
+                                                           - (N2Z + N2Z_1) * vbar[n]
+                                                           + N2Z_1 * vbar[n-1] )
                           )
-                    
-                    AC[n, n-1] = ( k[i]*ubar[n] + l[j]*vbar[n] ) * B_1 + P
-                    AC[n, n] = ( k[i]*ubar[n] + l[j]*vbar[n] ) * B + P
-                    AC[n, n+1] = ( k[i]*ubar[n] + l[j]*vbar[n] ) * Bt1 + P
-                    BD[n, n-1] = B_1
-                    BD[n, n] = B
-                    BD[n, n+1] = Bt1
-                    
+#                 print B_1, B, Bt1, P
+
+                    L[n, n-1] = R * B_1 + P
+                    L[n, n] = R * B + P
+                    L[n, n+1] = R * Bt1 + P
+                    G[n, n-1] = B_1
+                    G[n, n] = B
+                    G[n, n+1] = Bt1
+
                 else:
+
+                    D = .5 * dzf[n-1]**-1
+                    S = ( k[i] * ( (ubar[n]-ubar[n-1])*D - N2[n-1]/f0 * etay[1] )
+                                        + l[j] * ( (vbar[n]-vbar[n-1])*D + N2[n-1]/f0 * etax[1] ) )
+#                 print S
+
+                    L[n, n-1] = R * ( -D ) - S
+                    L[n, n] = R * D - S
+                    G[n, n-1] = - D
+                    G[n, n] = D     
                     
-                    D = dzf[n-1]**-1
-                    S = ( k[i] * ( (ubar[n]-ubar[n-1])*D - N2[n]/f_0**2 * etay[1] )
-                                + l[j] * ( (vbar[n]-vbar[n-1])*D - N2[n]/f_0**2 * etax[1] ) )
-                    
-                    AC[n, n-1] = ( ( k[i]*ubar[n] + l[j]*vbar[n] ) * ( -D ) 
-                                    - S
-                                    )
-                    AC[n, n] = ( ( k[i]*ubar[n] + l[j]*vbar[n] ) * D 
-                                  - S
-                                 )
-                    BD[n, n-1] = - D
-                    BD[n, n] = D
-                    
-            val, func = eigs( inv(BD).dot(AC), which='SM' )
+            #val, func = eigs( lil_matrix(lil_matrix(inv(G)).dot(L)), k=num, which=sort )   # default returns 6 eigenvectors
+            val, func = eigs( csc_matrix(csc_matrix(inv(csc_matrix(G))).dot(csc_matrix(L))), 
+                             k=num, which=sort )   # default returns 6 eigenvectors
 
             ###########
             # eigs returns complex values. For a linear-stability analysis, 
-            # we don't want the eigenfunction to be real
+            # we don't want the eigenvalues to be real
             ###########
             #tol = 1e-20
             #np.testing.assert_allclose(np.imag(func), 0, atol=tol)
             #np.testing.assert_allclose(np.imag(val), 0, atol=tol)
             #val = np.real(val)
             #func = np.real(func)
-                
-            #############
-            # they are often sorted and normalized, but not always
-            # so we have to do that here
-            # Sort them by the imaginary part of the eigenvalues
-            #############
-            p = np.argsort( val.imag )[::-1]
-            val = val[p]
-            func = func.real[:,p]
+
+            if i == 0 and j == 0:
+                omega = np.zeros( (len(val), len(l), len(k)), dtype=complex )
+                psi = np.zeros( (nz+1, len(val), len(l), len(k)), dtype=complex )
+            omega[:, j, i] = val
+            psi[:, :, j, i] = func  # Each column is the eigenfunction
             
-            # take the first four leading modes
-            omega[:, j, i] = val[:4]
-            psi1[:, j, i] = func[:, 0]
-            psi2[:, j, i] = func[:, 1]
-            psi3[:, j, i] = func[:, 2]
-            psi4[:, j, i] = func[:, 3]
-                
-    return k, l, zf, omega, psi1, psi2, psi3, psi4
+    ###########
+    # they are often sorted and normalized but not always,
+    # so we have to do that here.
+    # sort them by the imaginary part of the eigenvalues 
+    # (growth rate)
+    ###########
+    omega1d = omega.reshape( (len(val), len(k)*len(l)) ).imag.max(axis=1)
+    p = np.argsort(omega1d)[::-1]
+    omega = omega[p]
+    psi = psi[:, p]
+    
+    # return the first four leading modes
+    return k, l, zf, omega1d[p], omega[:4], psi[:, :4]
     
